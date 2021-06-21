ef4ba415937d0c93980c2f3712a48cce7a94e7c7
Can Yanas <can_yanas@icterra.com>
2021-06-21 17:40:28 +0300

    Hello World Project

---
+++ helloworld_can/src/test/java/helloworld_can/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld_can;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println("...Hello World...");
+	}
+
+}
